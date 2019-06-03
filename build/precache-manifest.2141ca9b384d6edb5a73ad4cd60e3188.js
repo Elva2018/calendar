fb348976048f1bfc829278920a12949e48378e23
Elva2018 <shelia1990@qq.com>
2019-06-03 21:20:29 +0800

    modify

---
+++ build/precache-manifest.2141ca9b384d6edb5a73ad4cd60e3188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "86254973c477bc088d34",
+    "url": "./static/js/main.e099beb2.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "./static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "86254973c477bc088d34",
+    "url": "./static/css/main.7dcf00a9.chunk.css"
+  },
+  {
+    "revision": "c839f51f835cff52addb2619f8c63979",
+    "url": "./index.html"
+  }
+];
